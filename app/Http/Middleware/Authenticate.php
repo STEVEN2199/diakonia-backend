bcaf6aa5051ec28c76c481663ad8543aec797dd1
STEVEN2199 <stevenguillermo_encalada@hotmail.com>
2023-10-02 17:31:18 -0500

    Register, login, logout
    Crecacion de login, register, logout y user para ver informacion de usuario junto con jwt
    
    Co-Authored-By: Ricardo David Salazar Secaira <56646823+ricdasal@users.noreply.github.com>

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Middleware;
+use closure;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
@@ -13,5 +14,14 @@ class Authenticate extends Middleware
     protected function redirectTo(Request $request): ?string
     {
         return $request->expectsJson() ? null : route('login');
+    }
+
+    public function handle($request, Closure $next, ...$guards){
+        if($jwt = $request->cookie('jwt')){
+            $request->headers->set('Authorization', 'Bearer '.$jwt);
+        }
+        $this-> authenticate($request, $guards);
+
+        return $next($request);
     }
 }
